f4b2731ab18075a5d289db43b1cde0d4061e1508
Tsuneo Kurihara <tsuneo.kurihara@codemotion.it>
2019-12-22 12:26:30 +0900

    loops and functions exercise

---
+++ 01-loops-and-functions.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func Sqrt(x float64) float64 {
+	z := 1.0
+	z1 := 1.0
+	i := 1
+	for ; i < 10; {
+		z1 -= (z*z - x) / (2*z)
+		
+		if math.Abs(z-z1) < 10e-6 {
+			fmt.Printf("Iterations: %d\n",i)
+			fmt.Printf("Standard library value: %f\n", math.Sqrt(x))
+			return z1
+		}
+		z = z1
+		i++
+	}
+	return z
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+}

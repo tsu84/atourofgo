becef52aab649b9647545958b29aabae89c3e689
Tsuneo Kurihara <tsuneo.kurihara@gmail.com>
2020-03-04 17:25:22 +0100

    08-rot13reader.go

---
+++ 08-rot13reader.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+)
+
+type rot13Reader struct {
+	r io.Reader
+}
+
+func (rot13 rot13Reader) Read(b []byte) (int, error) {
+	n, e := rot13.r.Read(b)
+	for i := 0; i < len(b); i++ {
+		switch {
+			case b[i] >= 65 && b[i] <= 65+13:
+				b[i] += 13
+			case b[i] >= 97 && b[i] <= 97+13:
+				b[i] += 13
+			case b[i] > 65+13 && b[i] <= 65+13+13:
+				b[i] -= 13
+			case b[i] > 97+13 && b[i] <= 97+13+13:
+				b[i] -= 13
+		}
+	}
+	return n, e
+}
+
+func main() {
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	r := rot13Reader{s}
+	io.Copy(os.Stdout, &r)
+}
